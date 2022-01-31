be02d2fee2d19f64655d8d10b17e0272490e4743
Darania Muralitharan <Darania@Daranias-MacBook-Air.local>
2022-01-31 12:12:00 +0000

    Login and Register Pages

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -48,7 +48,7 @@ const Newsletter = () => {
       <Title>Newsletter</Title>
       <Desc>Get timely updates from your favorite products.</Desc>
       <InputContainer>
-        <Input placeholder="Your email" />
+        <Input placeholder="Your Email" />
         <Button>
           <Send />
         </Button>
